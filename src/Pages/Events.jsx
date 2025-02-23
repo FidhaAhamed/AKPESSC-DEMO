b5566c24a96b146aee447af9208d1f98ea015044
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-02-23 20:29:18 +0530

    minor edit

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -50,7 +50,7 @@ const Events = () => {
         Events
       </motion.h2>
 
-+      {/* Event Cards */}
+      {/* Event Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto">
         {eventsData.map((event, index) => (
           <motion.div
